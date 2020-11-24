cd1f52da3d217cab190d6c09dd377d76782dacab
Marawan miro <marawanhoi@hotmail.com>
2020-11-24 09:12:55 +0100

    updated version

--- classes/placeGraph.py
+++ classes/placeGraph.py
@@ -4,6 +4,7 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,# NOQA
                                                NavigationToolbar2Tk)
 import csv
 import pandas as pd
+import matplotlib.animation as animation
 
 class PlaceGraph():
     def __init__(self, root, c, fig, x, y1, canid, byte):
@@ -22,3 +23,9 @@ class PlaceGraph():
         canvas.draw()
         canvas.get_tk_widget().place(x=root.winfo_screenwidth() *
                                      x, y=root.winfo_screenheight() * y1,anchor='center')
+
+    def graph(i):
+        data = pd.read_csv('dataFiles/dataCAN.csv')
+        xvals = data['HOUR']
+        yvals = data['MSG']
+        yvals = [int(i.split('/')[byte]) for i in yvals]
