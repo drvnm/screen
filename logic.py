e4974783ad92b47001e3aa7b09af953dfa959887
Venum <60921646+marawanmo@users.noreply.github.com>
2021-04-21 18:04:18 +0200

    Update logic.py

--- logic.py
+++ logic.py
@@ -9,18 +9,11 @@ import pandas as pd
 from classes.canReader import CanReader
 from classes.placeCanText import PlaceInfo
 import matplotlib.animation as animation
-
 from matplotlib.widgets import Button as GraphButton
-
 from classes.switchGifs import SwitchGifs
 import can
 
-
-
 root = Tk()
-
-
-
 
 # ===== niet veranderen
 
@@ -30,15 +23,13 @@ root.bind('<Escape>', quit)
 SCREENWIDTH = root.winfo_screenwidth()
 SCREENHEIGHT = root.winfo_screenheight()
 
-
 def quit(event):
     root.quit()
 
 
 root.attributes("-fullscreen", True)
 canvas = Canvas(root, height=1080, width=1920,
-                highlightthickness=0, background='#2E3347')
+              highlightthickness=0, background='#2E3347')
 canvas.pack(fill="both", expand=True)
-
 xpng = ImageTk.PhotoImage(Image.open('rsc/data.png'))
 
